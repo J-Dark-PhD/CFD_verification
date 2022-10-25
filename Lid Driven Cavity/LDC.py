5de564631c2691fb21914b594b1e5d14ddaafb27
J Dark <james.dark.95@gmail.com>
2022-10-25 06:28:15 +0000

    updated formulation and added comments

--- Lid Driven Cavity/LDC.py
+++ Lid Driven Cavity/LDC.py
@@ -18,8 +18,7 @@ noslip = DirichletBC(W.sub(0), (0, 0), fixed_wall_locations)
 
 u_x = 1
 velocity_x = Constant((u_x, 0.0))
-top_velocity = DirichletBC(W.sub(0), velocity_x,
-                           lid_location)
+top_velocity = DirichletBC(W.sub(0), velocity_x, lid_location)
 
 bcu = [top_velocity, noslip]
 
@@ -33,42 +32,39 @@ u, p = split(up)
 
 f = Constant((0, 0))
 
-
-for top_velocity in [400, 1000, 2500, 5000, 10000]:
+# need to ramp up the values feeding into the function as a first
+# guess for the next run to ease the solving
+for top_velocity in [400, 1000, 2000, 4000, 5000, 7000, 9000, 10000]:
 
     up.assign(up_old)
     rho_0 = 1
     mu = 1
     # top_velocity = 1
-    Reynolds = rho_0*top_velocity/mu
-    print("Doing it for Re={}".format(Reynolds))
+    Reynolds = rho_0 * top_velocity / mu
+    print("Running case Re={}".format(Reynolds))
     velocity_x.assign(Constant((top_velocity, 0)))
 
-    # Solver
+    # ##### Solver ##### #
 
+    # CFD momentum
     F = (
-        rho_0*inner(grad(u)*u, v)*dx + mu*inner(grad(u), grad(v))*dx
-        - inner(p, div(v))*dx + inner(q, div(u))*dx
-        + inner(f, v)*dx
+        rho_0 * inner(grad(u), grad(v)) * dx
+        - inner(p, div(v)) * dx
+        + mu * inner(dot(grad(u), u), v) * dx
     )
 
-    F += inner(q, div(u))*dx
+    # CFD continuity
+    F += inner(div(u), q) * dx
 
     solve(F == 0, up, bcu)
 
     up_old.assign(up)
     u_export.assign(up)
-    u_export, p_export = u_export.split()
-    XDMFFile("Results/Re={:.1f}_u.xdmf".format(Reynolds)).write(u_export)
-    XDMFFile("Results/Re={:.1f}_p.xdmf".format(Reynolds)).write(p_export)
 
+    # only export the values needed
+    interested_values = [400, 1000, 5000, 10000]
 
-# ##### Adaptive Mesh Refinement Option ##### #
-# M = u[0]**2*dx
-# epsilon_M = 1.e-6
-# epsilon_M = Re*1.e-3
-
-# solver = AdaptiveNonlinearVariationalSolver(problem, M)
-# solver.parameters["nonlinear_variational_solver"]['newton_solver']["maximum_iterations"] = 10
-
-# solver.solve(epsilon_M)
+    if top_velocity in interested_values:
+        u_export, p_export = u_export.split()
+        XDMFFile("Results/Re={:.1f}_u.xdmf".format(Reynolds)).write(u_export)
+        XDMFFile("Results/Re={:.1f}_p.xdmf".format(Reynolds)).write(p_export)
