dc145273d11f6741b7927bf5044087ad761ab704
J Dark <james.dark.95@gmailcom>
2023-01-21 10:48:32 +0000

    new geom, solving methods and parametric testing

--- MHD/mhd.py
+++ MHD/mhd.py
@@ -1,16 +1,15 @@
 from fenics import *
 
-N = 15
-mesh = UnitCubeMesh(N, N, N)
+mesh = BoxMesh(Point(0,0,0), Point(20, 2, 2), 20, 40, 40)
 
-V_ele = VectorElement("CG", mesh.ufl_cell(), 3)
+V_ele = VectorElement("CG", mesh.ufl_cell(), 2)
 Q_ele = FiniteElement("CG", mesh.ufl_cell(), 1)
 W = FunctionSpace(mesh, MixedElement([V_ele, Q_ele, Q_ele]))
 
-fixed_wall_locations = "near(x[1], 0.) | near(x[1], 1.) | near(x[2], 0.) | near(x[2], 1.)"
+fixed_wall_locations = "near(x[1], 0.) | near(x[1], 2.) | near(x[2], 0.) | near(x[2], 2.)"
 inlet = "near(x[0], 0.)"
-outlet = "near(x[0], 1.)"
-hartmann_walls = "near(x[1], 0.) | near(x[1], 1.)"
+outlet = "near(x[0], 20.)"
+hartmann_walls = "near(x[2], 0.) | near(x[2], 2.)"
 
 velocity_inlet = DirichletBC(W.sub(0), Constant((10, 0, 0)), inlet)
 pressure_outlet = DirichletBC(W.sub(1), Constant(0), outlet)
@@ -31,24 +30,43 @@ func_old = Function(W)
 export = Function(W)
 
 # Momentum
-# F = (
-# inner(dot(grad(u), u), v) * dx
-# - inner(p, div(v)) * dx
-# + inner(grad(u), grad(v)) * dx
-# + N * (inner(cross(B, grad(phi)), v) * dx
-# + inner(u * dot(B, B), v) * dx
-# - inner(B * dot(B, u), v) * dx)
-# )
+F = (
+inner(dot(grad(u), u), v) * dx
+- inner(p, div(v)) * dx
++ inner(grad(u), grad(v)) * dx
++ N * (inner(cross(B, grad(phi)), v) * dx
++ inner(u * dot(B, B), v) * dx
+- inner(B * dot(B, u), v) * dx)
+)
 
-# # CFD continuity
-# F += - inner(q, div(u)) * dx
+# CFD continuity
+F += - inner(q, div(u)) * dx
 
-# # electric continuity
-# F += (inner(grad(phi), grad(q_2)) * dx
-# - inner(dot(B, curl(u)) + dot(u, curl(B)), q_2) * dx
-# )
+# electric continuity
+F += (inner(grad(phi), grad(q_2)) * dx
+- inner(dot(B, curl(u)) + dot(u, curl(B)), q_2) * dx
+)
 
-# solve(F == 0, func, bcu, solver_parameters={'newton_solver': {'linear_solver': 'mumps'}})
+solve(
+    F == 0, 
+    func,
+    bcu_conductive,
+    solver_parameters={
+        'symmetric':True,
+        'newton_solver': {
+        'linear_solver': 'gmres',
+        'preconditioner': 'amg',
+        'relative_tolerance': 1e-06,
+        'krylov_solver': {
+            'absolute_tolerance':1e-6,
+            'relative_tolerance': 1e-6,
+            'maximum_iterations': 10000,
+            'nonzero_initial_guess': False,
+            'error_on_nonconvergence': True,
+            'monitor_convergence': True,
+            'report': True,
+            'divergence_limit': 10000.0
+    }}})
 
 # func_old.assign(func)
 # export.assign(func)
@@ -60,39 +78,39 @@ export = Function(W)
 insulated_test_values = [0, 10, 30, 60, 100, 500, 1000]
 conductive_test_values = [0, 10, 30, 60, 100]
 
-for Hartmann_number in insulated_test_values:
-    print("Running insulated case Ha={}".format(Hartmann_number))
-    func.assign(func_old)    
-    Ha = Constant(Hartmann_number)
-    N = Ha**2
+# for Hartmann_number in insulated_test_values:
+#     print("Running insulated case Ha={}".format(Hartmann_number))
+#     func.assign(func_old)    
+#     Ha = Constant(Hartmann_number)
+#     N = Ha**2
 
-    # Momentum
-    F = (
-    inner(dot(grad(u), u), v) * dx
-    - inner(p, div(v)) * dx
-    + inner(grad(u), grad(v)) * dx
-    + N * (inner(cross(B, grad(phi)), v) * dx
-    + inner(u * dot(B, B), v) * dx
-    - inner(B * dot(B, u), v) * dx)
-    )
+#     # Momentum
+#     F = (
+#     inner(dot(grad(u), u), v) * dx
+#     - inner(p, div(v)) * dx
+#     + inner(grad(u), grad(v)) * dx
+#     + N * (inner(cross(B, grad(phi)), v) * dx
+#     + inner(u * dot(B, B), v) * dx
+#     - inner(B * dot(B, u), v) * dx)
+#     )
 
-    # CFD continuity
-    F += - inner(q, div(u)) * dx
+#     # CFD continuity
+#     F += - inner(q, div(u)) * dx
 
-    # electric continuity
-    F += (inner(grad(phi), grad(q_2)) * dx
-    - inner(dot(B, curl(u)) + dot(u, curl(B)), q_2) * dx
-    )
+#     # electric continuity
+#     F += (inner(grad(phi), grad(q_2)) * dx
+#     - inner(dot(B, curl(u)) + dot(u, curl(B)), q_2) * dx
+#     )
 
-    solve(F == 0, func, bcu_insulated, solver_parameters={'newton_solver': {'linear_solver': 'mumps'}})
+#     solve(F == 0, func, bcu_insulated, solver_parameters={'newton_solver': {'linear_solver': 'mumps', 'relative_tolerance': 1e-04, 'relaxation_parameter': 0.3}})
 
-    func_old.assign(func)
-    export.assign(func)
-    u_export, p_export, phi_export = export.split()
-    results_folder = "Results/fully_insulated/Ha={}/".format(Hartmann_number)
-    XDMFFile(results_folder + "u.xdmf").write(u_export)
-    XDMFFile(results_folder + "p.xdmf").write(p_export)
-    XDMFFile(results_folder + "phi.xdmf").write(phi_export)
+#     func_old.assign(func)
+#     export.assign(func)
+#     u_export, p_export, phi_export = export.split()
+#     results_folder = "Results/fully_insulated/Ha={}/".format(Hartmann_number)
+#     XDMFFile(results_folder + "u.xdmf").write(u_export)
+#     XDMFFile(results_folder + "p.xdmf").write(p_export)
+#     XDMFFile(results_folder + "phi.xdmf").write(phi_export)
 
 
 for Hartmann_number in conductive_test_values:
@@ -119,7 +137,7 @@ for Hartmann_number in conductive_test_values:
     - inner(dot(B, curl(u)) + dot(u, curl(B)), q_2) * dx
     )
 
-    solve(F == 0, func, bcu_conductive, solver_parameters={'newton_solver': {'linear_solver': 'mumps'}})
+    solve(F == 0, func, bcu_conductive, solver_parameters={'newton_solver': {'linear_solver': 'mumps', 'relative_tolerance': 1e-06}})
 
     func_old.assign(func)
     export.assign(func)
